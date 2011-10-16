78a934875afe8130495114b01b6ded2542305e3a
Cheol Kang <kang@cheol.net>
2011-10-16 19:58:46 +0900

    Server booting script

---
+++ start_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Usage:
+# ./start_server.sh
+#  - run server on port 3000(default)
+#
+# ./start_server.sh 3001
+#  - run server on port 3001
+nohup node app.js $1 >> server.log 2>&1 &
